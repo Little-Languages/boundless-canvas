25504af9504f48701acc8280aeb34002ddcafae3
“chrisshank” <chris.shank.23@gmail.com>
2024-05-15 23:28:58 -0700

    implement custom attributes

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 > This library is still in development. 🚧 It's not published to npm yet. Expect breaking changes in minor releases until things stabilize in 1.0. Use in production at your own risk.
 
 Documentation is WIP, in the meantime check out the [demos](https://github.com/Little-Languages/boundless-canvas/tree/main/demo) for how to use this library.
+
+## Inspiration
+
+- [Shapes](https://www.smashingmagazine.com/2024/05/modern-guide-making-css-shapes/)
+- [Custom Attributes](https://github.com/WICG/webcomponents/issues/1029)
--- src/base-element.ts
+++ src/base-element.ts
@@ -1,36 +1,74 @@
-interface CustomAttribute {
-  readonly name: string;
-  readonly ownerElement: Element;
-  readonly value: string;
-  connectedCallback(): void;
-  disconnectedCallback(): void;
-  changedCallback(): void;
+export class CustomAttribute {
+  constructor(readonly ownerElement: Element, readonly name: string) {}
+
+  // @ts-ignore
+  connectedCallback(value: string): void {}
+
+  disconnectedCallback(): void {}
+
+  // @ts-ignore
+  changedCallback(oldValue: string, newValue: string): void {}
+}
+
+type CustomAttributeConstructor = typeof CustomAttribute;
+
+export class AttributeRegistry {
+  _attributesMap = new Map<string, CustomAttributeConstructor>();
+
+  define(name: string, customAttribute: CustomAttributeConstructor) {
+    if (!name.includes('-')) throw new Error(`Custom attribute '${name}' must include a hyphen.`);
+
+    this._attributesMap.set(name, customAttribute);
+  }
+
+  get(name: string): CustomAttributeConstructor | undefined {
+    return this._attributesMap.get(name);
+  }
 }
 
 export class BaseElement extends HTMLElement {
-  static tagName = 'abstract-arrow';
+  static tagName = 'base-element';
 
   static register() {
     customElements.define(this.tagName, this);
   }
 
-  static #attributesMap = new Map<string, CustomAttribute>();
-
-  static attributeRegistry = {
-    define: (name: string, customAttribute: CustomAttribute) => {
-      if (!name.includes('-')) throw new Error(`Custom attribute '${name}' must include a hyphen.`);
-
-      this.#attributesMap.set(name, customAttribute);
-    },
-
-    get: (name: string): CustomAttribute | undefined => {
-      return this.#attributesMap.get(name);
-    },
-  };
+  static attributeRegistry = new AttributeRegistry();
 
   static get observedAttributes(): string[] {
-    return Array.from(this.#attributesMap.keys());
+    return Array.from(this.attributeRegistry._attributesMap.keys());
   }
 
-  attributeChangedCallback(name: string, oldValue: string, newValue: string) {}
+  get #attributeRegistry() {
+    return (this.constructor as typeof BaseElement).attributeRegistry;
+  }
+
+  #attributes = new Map<string, CustomAttribute>();
+
+  disconnectedCallback() {
+    this.#attributes.forEach((customAttribute) => customAttribute.disconnectedCallback());
+    this.#attributes.clear();
+  }
+
+  attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
+    let customAttribute = this.#attributes.get(name);
+
+    if (customAttribute === undefined) {
+      const attributeConstructor = this.#attributeRegistry.get(name);
+
+      if (attributeConstructor === undefined) return;
+
+      customAttribute = new attributeConstructor(this, name);
+      this.#attributes.set(name, customAttribute);
+    }
+
+    if (oldValue === null) {
+      customAttribute.connectedCallback(newValue!);
+    } else if (newValue === null) {
+      customAttribute.disconnectedCallback();
+      this.#attributes.delete(name);
+    } else {
+      customAttribute.changedCallback(oldValue, newValue);
+    }
+  }
 }
